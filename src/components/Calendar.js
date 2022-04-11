0b7195515f6ffb681a89fc49a6c87d2fb853bc33
pablolucianop <pablolucianop@gmail.com>
2022-04-11 19:11:46 -0300

    fix index

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Calendar.css'
+import {
+  startOfWeek,
+  endOfWeek,
+  startOfMonth,
+  endOfMonth,
+  eachDayOfInterval,
+  eachWeekOfInterval,
+  format as formatFn,
+} from 'date-fns'
 
 function Calendar() {
+  const now = new Date()
+  const getDayNumber = (date = now) => formatFn(date, 'd')
+  console.log(getDayNumber())
   const greeting = 'Calendar'
 
-  return <h1>{greeting}</h1>
+  return <h1>{}</h1>
 }
 
 export default Calendar
