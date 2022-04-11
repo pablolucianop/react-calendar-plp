878d7420980f97549ce01933d82d7675cd01daa7
pablolucianop <pablolucianop@gmail.com>
2022-04-11 18:34:43 -0300

    basic Layout

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Calendar from './Calendar'
+function App() {
+  return (
+    <div className="App">
+      {/* <Provider store={store}> */}
+      <div className="App">
+        <Calendar />
+      </div>
+      {/* </Provider> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Calendar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Calendar.css'
+
+function Calendar() {
+  const greeting = 'Calendar'
+
+  return <h1>{greeting}</h1>
+}
+
+export default Calendar
