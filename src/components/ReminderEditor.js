c6002ecf218486039ec94a99594f5ba4e0d6fb79
pablolucianop <pablolucianop@gmail.com>
2022-04-12 06:15:29 -0300

    add primitive color

--- src/components/ReminderEditor.js
+++ src/components/ReminderEditor.js
@@ -3,9 +3,10 @@ import './ReminderEditor.css'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import { CirclePicker } from 'react-color'
 function ReminderEditor() {
   const [show, setShow] = useState(false)
-
+  const [color, setColor] = useState('#ff0000')
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
@@ -16,18 +17,37 @@ function ReminderEditor() {
       </Button>
 
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+        <Form
+          style={{
+            backgroundColor: color,
+            transition: 'ease all 500ms',
+          }}
+        >
+          <Form.Group className="mb-3" controlId="formBasicReminder">
+            <Form.Label>Reminder</Form.Label>
+            <Form.Control type="email" placeholder="Enter Reminder" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control type="email" placeholder="Enter City" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formCity">
+            <Form.Label>Color</Form.Label>
+            <CirclePicker
+              color={color}
+              onChangeComplete={(color) => {
+                setColor(color.hex)
+              }}
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
+          <Button variant="primary" type="submit">
+            cancel
           </Button>
-        </Modal.Footer>
+        </Form>
       </Modal>
     </>
   )
