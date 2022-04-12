97493d80205e255e544065f6e4f0a5f40b584311
pablolucianop <pablolucianop@gmail.com>
2022-04-12 11:55:12 -0300

    add simplest redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,98 @@
 // import { Provider } from 'react-redux'
+import React from 'react'
 import './App.css'
 import Calendar from './Calendar'
+import { Provider, connect } from 'react-redux'
+import { createStore } from 'redux'
+
 function App() {
   const calendar = <Calendar />
 
+  // Redux:
+  const ADD = 'ADD'
+
+  const addMessage = (message) => {
+    return {
+      type: ADD,
+      message: message,
+    }
+  }
+
+  const messageReducer = (state = [], action) => {
+    switch (action.type) {
+      case ADD:
+        return [...state, action.message]
+      default:
+        return state
+    }
+  }
+
+  const store = createStore(messageReducer)
+
+  // React:
+  class Presentational extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        input: '',
+      }
+      this.handleChange = this.handleChange.bind(this)
+      this.submitMessage = this.submitMessage.bind(this)
+    }
+    handleChange(event) {
+      this.setState({
+        input: event.target.value,
+      })
+    }
+    submitMessage() {
+      this.props.submitNewMessage(this.state.input)
+      this.setState({
+        input: '',
+      })
+    }
+    render() {
+      return (
+        <div>
+          <h2>Type in a new Message:</h2>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <br />
+          <button onClick={this.submitMessage}>Submit</button>
+          <ul>
+            {this.props.messages.map((message, idx) => {
+              return <li key={idx}>{message}</li>
+            })}
+          </ul>
+        </div>
+      )
+    }
+  }
+
+  // React-Redux
+
+  const mapStateToProps = (state) => {
+    return { messages: state }
+  }
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      submitNewMessage: (message) => {
+        dispatch(addMessage(message))
+      },
+    }
+  }
+
+  const Container = connect(mapStateToProps, mapDispatchToProps)(Presentational)
+
   return (
     // <Provider store={store}>
-    <div className="App">
-      <Calendar />
-    </div>
+
+    <Provider store={store}>
+      <Container />
+      <div className="App">
+        <Calendar />
+      </div>
+    </Provider>
+
     // </Provider>
   )
 }
