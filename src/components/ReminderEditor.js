854d7f0464ec5b60b35d6ac7e9edbbd593402d22
pablolucianop <pablolucianop@gmail.com>
2022-04-14 04:13:37 -0300

    limit palette

--- src/components/ReminderEditor.js
+++ src/components/ReminderEditor.js
@@ -65,6 +65,27 @@ function ReminderEditor({ dispatch, messages, setShow }) {
     handleClose(e)
   }
 
+  const colors = [
+    '#f44336',
+    '#e91e63',
+    // '#9c27b0',
+    // '#673ab7',
+    // '#3f51b5',
+    // '#2196f3',
+    '#03a9f4',
+    // '#00bcd4',
+    // '#009688',
+    // '#4caf50',
+    '#8bc34a',
+    '#cddc39',
+    // '#ffeb3b',
+    '#ffc107',
+    // '#ff9800',
+    // '#ff5722',
+    // '#795548',
+    // '#607d8b',
+  ]
+
   return (
     <div className="reminder-editor">
       <Form.Group className="mb-3" controlId="formBasicReminder">
@@ -115,8 +136,10 @@ function ReminderEditor({ dispatch, messages, setShow }) {
       </Form.Group>
       <Form.Group className="mb-3" controlId="formCity">
         <Form.Label>Color</Form.Label>
+
         <CirclePicker
           color={color}
+          colors={colors}
           onChangeComplete={(color) => {
             setColor(color.hex)
           }}
