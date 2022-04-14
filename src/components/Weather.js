91a37aedde6778e410d1f3c0c061f9424a76563c
pablolucianop <pablolucianop@gmail.com>
2022-04-14 19:05:25 -0300

    fix: weather get 2

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,36 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-// const Calendar2 = () => {
-//   const apiKey = '88311788ed96ee764097bb269c07c5f7'
-//   let iconcode
-//   let icon
-//   let mainWeatherData
-
-//   const getLatLongFromCity = async (city, apiKey) => {
-//     const response = await fetch(
-//       `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-//     )
-//     const data = await response.json()
-//     console.log('data', data)
-//     iconcode = data.weather[0].icon
-//     console.log('iconcode', iconcode)
-
-//     icon = (
-//       <img
-//         src={`http://openweathermap.org/img/w/${iconcode}.png`}
-//         alt="Weather icon"
-//       ></img>
-//     )
-//     mainWeatherData = data.weather[0].main
-//     console.log(data.weather[0].main)
-//     return data
-//   }
-
-//   getLatLongFromCity('Rosario', apiKey)
-
-//   return <div>hello{icon}</div>
-// }
 let icon
 
 const Weather = ({ city }) => {
@@ -58,7 +28,7 @@ const Weather = ({ city }) => {
         ></img>
       )
       console.log('data', data)
-      this.setState({ serverResponse: data })
+      // this.setState({ serverResponse: data })
     }
     fetchData()
   }, [])
