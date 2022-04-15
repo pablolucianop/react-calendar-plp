f6532445cdd489c0fbf761b32d195987e1511e18
pablolucianop <pablolucianop@gmail.com>
2022-04-15 08:48:10 -0300

    fix: axios catch errors

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -14,18 +14,26 @@ const Weather = ({ city, date }) => {
   useEffect(() => {
     async function fetchData() {
       console.log('Weather: ', city)
-      const latLong = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
-      )
+      const latLong = await axios
+        .get(
+          `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`
+        )
+        .catch((error) => {
+          console.log(error.response)
+        })
 
       const lat = latLong.data[0].lat
       const lon = latLong.data[0].lon
 
       console.log('latLong: ', latLong.data[0].lat)
 
-      const res = await axios.get(
-        `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
-      )
+      const res = await axios
+        .get(
+          `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+        )
+        .catch((error) => {
+          console.log(error.response)
+        })
       const weatherInRange = res.data.list.filter(isInRange)
       const mainWeatherData = weatherInRange[0].weather[0].main
       setSpecificWeather(mainWeatherData)
